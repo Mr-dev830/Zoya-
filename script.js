1dc6eaedb5c82900352ea8ff9d2bde20067929c5
Mr-dev830 <fankit830@gmail.com>
2026-03-13 12:21:56 +0530

    Add birthday surprise features with code check

---
+++ script.js
@@ -0,0 +1,78 @@
+const secretCode = "1203"
+
+function checkCode(){
+
+let input=document.getElementById("codeInput").value
+
+if(input===secretCode){
+
+document.getElementById("lockScreen").style.display="none"
+
+document.getElementById("mainSite").style.display="block"
+
+}else{
+
+document.getElementById("error").innerText="Wrong code ❤️"
+
+}
+
+}
+
+
+function openGift(){
+
+document.getElementById("music").play()
+
+startTyping()
+
+confetti()
+
+}
+
+
+function startTyping(){
+
+let text="You are the most beautiful thing that ever happened to me. Happy Birthday my love ❤️"
+
+let i=0
+
+function typing(){
+
+if(i<text.length){
+
+document.getElementById("typing").innerHTML+=text.charAt(i)
+
+i++
+
+setTimeout(typing,40)
+
+}
+
+}
+
+typing()
+
+}
+
+
+function confetti(){
+
+for(let i=0;i<40;i++){
+
+let heart=document.createElement("div")
+
+heart.innerHTML="❤️"
+
+heart.style.position="fixed"
+
+heart.style.left=Math.random()*100+"vw"
+
+heart.style.top="-10px"
+
+heart.style.fontSize="20px"
+
+document.body.appendChild(heart)
+
+}
+
+}
